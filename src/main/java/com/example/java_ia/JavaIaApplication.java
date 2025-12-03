a8a551e9aa8f6674a5587644762e3d4c6c036afc
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2025-12-03 08:37:22 -0300

    :tada:	init Projeto criado
    :tada:	init Projeto criado

---
+++ src/main/java/com/example/java_ia/JavaIaApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_ia/JavaIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
