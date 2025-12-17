66c6fb89172e4f1cb01a49824200ef60a8d4b6f2
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2025-12-17 07:54:54 -0300

    continua..
    continua..

--- src/main/java/com/example/java_ia/JavaIaApplication.java
+++ src/main/java/com/example/java_ia/JavaIaApplication.java
@@ -3,11 +3,18 @@ package com.example.java_ia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class JavaIaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaIaApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(JavaIaApplication.class, args);
+		System.out.println("hello");
+		Scanner scanner;
+
+
+
 	}
+
 
 }
