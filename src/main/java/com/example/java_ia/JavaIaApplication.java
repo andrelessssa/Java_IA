903cdf107075dcaae87f6a4506899999325c56d5
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2026-01-06 08:25:18 -0300

    teste
    teste

--- src/main/java/com/example/java_ia/JavaIaApplication.java
+++ src/main/java/com/example/java_ia/JavaIaApplication.java
@@ -10,8 +10,25 @@ public class JavaIaApplication {
 
 	public static void main(String[] args) {SpringApplication.run(JavaIaApplication.class, args);
 		System.out.println("hello");
-		Scanner scanner;
 
+
+		byte number = 1;
+		long nunb = 1L;
+		float num = 1.0f;
+		var nu = 1.0d;
+		char cha = 'a';
+
+		//var scanner = new Scanner(System.in);
+		//System.out.println("Quanto é 2 + 2");
+		//var result = scanner.nextInt();
+		//System.out.printf("O resultado é 4, e voce acertou? (%s)", result == 4);
+
+
+		var scanner = new Scanner(System.in); // inicializada
+		System.out.println("Quanto é 2 + 2"); // ln quebra a linha
+		var result = scanner.nextInt();
+		var isRight = result == 4;
+		System.out.printf("O resultado é 4, e voce acertou? (%s)", result == 4); // o f é para usar o %s
 
 
 	}
